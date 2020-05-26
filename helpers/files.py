242d541bd4548291c50da35194dc7ab9349886a4
Gabriel Chamon Araujo <gchamon@live.com>
2020-05-26 10:48:18 -0300

    implemented s3 sync

--- helpers/files.py
+++ helpers/files.py
@@ -3,7 +3,6 @@ import os
 import psutil
 
 from logger import logging
-from s3_batch import S3_BATCH_BUCKETS_FOLDER
 
 
 def get_file_size_mb(file_path):
@@ -30,21 +29,22 @@ def get_filename_extension(filename):
 s3_opened_files = None
 
 
-def filter_opened_files(files):
+def filter_opened_files(files, base_folder, warnings=True):
     global s3_opened_files
     if s3_opened_files is None:
         s3_opened_files = [opened_file
-                           for opened_files_by_process in get_opened_files()
+                           for opened_files_by_process in get_opened_files(warnings)
                            for opened_file in opened_files_by_process
-                           if S3_BATCH_BUCKETS_FOLDER in opened_file]
+                           if base_folder in opened_file]
     return [file for file in files
             if file not in s3_opened_files]
 
 
-def get_opened_files():
+def get_opened_files(warnings=True):
     for pid in psutil.pids():
         try:
             yield (file[0] for file in psutil.Process(pid).open_files())
         except psutil.AccessDenied as e:
-            logging.warning("Access denied while getting process opened files")
-            logging.warning(str(e))
+            if warnings:
+                logging.warning("Access denied while getting process opened files")
+                logging.warning(str(e))
--- s3_sync.py
+++ s3_sync.py
@@ -1,15 +1,53 @@
 import os
+import shutil
+from tempfile import TemporaryDirectory
 
+from helpers.files import filter_opened_files
 from helpers.files import list_directories
+from helpers.s3 import get_s3_file_prefix, sync_bucket_folder
 from logger import logging
 
 S3_SYNC_BUCKETS_FOLDER = os.environ.get('S3_SYNC_BUCKETS_FOLDER', os.path.expandvars("$HOME/sync_buckets"))
 
 
 def main():
-    logging.info("Start buckets sync")
+    logging.info("Start s3 sync")
     os.makedirs(S3_SYNC_BUCKETS_FOLDER, exist_ok=True)
     s3_buckets = list_directories(S3_SYNC_BUCKETS_FOLDER)
+    move_ready_files_to_temp_dir_and_sync(s3_buckets)
+    logging.info("Done")
+
+
+def move_ready_files_to_temp_dir_and_sync(s3_buckets):
+    with TemporaryDirectory() as temporary_directory:
+        buckets_to_sync = []
+
+        # this step moves files to temporary directory
+        for bucket in s3_buckets:
+            bucket_directory = os.path.join(S3_SYNC_BUCKETS_FOLDER, bucket)
+            for directory, subdirectories, files in os.walk(bucket_directory):
+                if files:
+                    logging.info(directory)
+                    logging.info(files)
+                    move_ready_files_to_temp_dir(bucket, directory, files, temporary_directory)
+                    buckets_to_sync.append(bucket)
+
+        # this step syncs buckets to s3. aws cli is used for convenience
+        for bucket in buckets_to_sync:
+            sync_bucket_folder(bucket, temporary_directory)
+
+
+def move_ready_files_to_temp_dir(bucket, directory, files, temporary_directory):
+    full_path_files = (os.path.join(directory, file)
+                       for file in files)
+    files_ready_to_sync = filter_opened_files(full_path_files, S3_SYNC_BUCKETS_FOLDER, warnings=False)
+    s3_file_prefix = get_s3_file_prefix(bucket, directory, S3_SYNC_BUCKETS_FOLDER)
+    destination_folder = os.path.join(temporary_directory, bucket, s3_file_prefix)
+    os.makedirs(destination_folder, exist_ok=True)
+    for file in files_ready_to_sync:
+        filename = os.path.basename(file)
+        destination_file = os.path.join(destination_folder, filename)
+        shutil.move(file, destination_file)
 
 
 if __name__ == '__main__':
